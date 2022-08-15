04a0210561c785fb3072510f4e5cfb1c3d106fda
Terran Kartik Tellus <91951646+TerranKartikTellus@users.noreply.github.com>
2022-08-15 22:34:24 +0530

    nav

--- components/common/navbar.js
+++ components/common/navbar.js
@@ -8,19 +8,29 @@ export default function nav(){
   );
 }
 function List(){
-  
+
   return(
     <div className="bg-green500 w-3/5 flex flex-row items-center justify-evenly">
-      <Link href={"/"}><a className="  font-sans tracking-wider  ">Latest</a></Link>
-      <Link href={"/"}><a className="  font-sans tracking-wider  ">About</a></Link>
-      <Link href={"/"}><a className="  font-sans tracking-wider  ">Sponsor</a></Link>
+      <Link href={"/latest"}>
+        <a className="  font-sans tracking-wider  ">Latest</a></Link>
+      <Link href={"/about"}>
+        <a className="  font-sans tracking-wider  ">About</a></Link>
+      <Link href={"/sponsor"}>
+        <a className="  font-sans tracking-wider  ">Sponsor</a></Link>
 
 
-      <Link href={"/"}><a className="  font-sans tracking-wider  ">HOME</a></Link>
 
-      <Link href={"/"}><a className="  font-sans tracking-wider  ">Galary</a></Link>
-      <Link href={"/"}><a className="  font-sans tracking-wider  ">Galary</a></Link>
-      <Link href={"/"}><a className="  font-sans tracking-wider  ">Contact</a></Link>
+
+      <Link href={"/"}>
+        <a className="  font-sans tracking-wider  ">HOME</a></Link>
+
+
+      <Link href={"/galary"}>
+        <a className="  font-sans tracking-wider  ">Galary</a></Link>
+      <Link href={"/contact"}>
+        <a className="  font-sans tracking-wider  ">Contact</a></Link>
+      <Link href={"/help"}>
+        <a className="  font-sans tracking-wider  ">Help</a></Link>
     </div>
   );
 }
