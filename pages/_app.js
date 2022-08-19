8fbd3f07bd8b53d390788d8c88bd3163f43a19f8
Terran Kartik Tellus <91951646+TerranKartikTellus@users.noreply.github.com>
2022-08-19 19:34:30 +0530

    added social media handles

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import Nav from '../components/common/navbar'
 import { useRouter } from 'next/router'
+import Link from 'next/link';
 
 
 
@@ -15,7 +16,6 @@ function MyApp({ Component, pageProps }) {
   return <main className=' font-Maven bg-black relative h-full w-full'>
 
 
-
     <video id="myVideo" preload="auto"  className="bg-cover opacity-50 saturate-200  hidden md:block object-cover w-full h-full absolute top-0 left-0"  autoPlay loop muted >
       <source  src="/live/mylivewallpapers.com-Spaceman-with-Dog.mp4" type="video/mp4" />
       {/* <source src="movie.ogg" type="video/ogg" /> */}
@@ -28,6 +28,7 @@ function MyApp({ Component, pageProps }) {
       <div className='bg-backgroundSM h-full  w-full bg-cover'></div>
     </video>
     <div className='bg-black/40 w-full  h-full absolute top-0 left-0 animate-'>.</div>
+<SocialMedia></SocialMedia>
     <div className='scale-90 rounded-full shadowd shadow-gray-400/80 drop-shadow-2xl'>
         <div className='hden'>
           
@@ -39,3 +40,31 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp
+
+function SocialMedia(){
+  return(
+    <div className='fixed flex flex-row items-center justify-center space-x-2 opacity-80 top-2 left-auto right-auto text-gray-100 w-full '>
+        <div>
+          <Link 
+          href="mailto:email-to@gmail.com?cc=email-cc@gmail.com&bcc=email-bcc@gmail.com&subject=Subject Using Mailto.co.uk&body=Email Using Body">
+          <a target="_blank" className='opacity-60 transition-all duration-300 ease-in-out hover:opacity-75'><svg className='invert  w-7 h-7' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2.917 16.083c-2.258 0-4.083-1.825-4.083-4.083s1.825-4.083 4.083-4.083c1.103 0 2.024.402 2.735 1.067l-1.107 1.068c-.304-.292-.834-.63-1.628-.63-1.394 0-2.531 1.155-2.531 2.579 0 1.424 1.138 2.579 2.531 2.579 1.616 0 2.224-1.162 2.316-1.762h-2.316v-1.4h3.855c.036.204.064.408.064.677.001 2.332-1.563 3.988-3.919 3.988zm9.917-3.5h-1.75v1.75h-1.167v-1.75h-1.75v-1.166h1.75v-1.75h1.167v1.75h1.75v1.166z"/></svg></a>
+          </Link>
+        
+        </div>
+
+        <div>
+          <Link href="https://www.linkedin.com/company/teamautoz/">
+          <a target="_blank" className='opacity-60 transition-all duration-300 ease-in-out hover:opacity-75'><svg className='invert  w-7 h-7' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2 16h-2v-6h2v6zm-1-6.891c-.607 0-1.1-.496-1.1-1.109 0-.612.492-1.109 1.1-1.109s1.1.497 1.1 1.109c0 .613-.493 1.109-1.1 1.109zm8 6.891h-1.998v-2.861c0-1.881-2.002-1.722-2.002 0v2.861h-2v-6h2v1.093c.872-1.616 4-1.736 4 1.548v3.359z"/></svg></a>
+          </Link>
+        
+        </div>
+
+        <div>
+          <Link href="https://www.instagram.com/teamautoz">
+          <a target="_blank" className='opacity-60 transition-all duration-300 ease-in-out hover:opacity-75'><svg className='invert  w-7 h-7' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M14.829 6.302c-.738-.034-.96-.04-2.829-.04s-2.09.007-2.828.04c-1.899.087-2.783.986-2.87 2.87-.033.738-.041.959-.041 2.828s.008 2.09.041 2.829c.087 1.879.967 2.783 2.87 2.87.737.033.959.041 2.828.041 1.87 0 2.091-.007 2.829-.041 1.899-.086 2.782-.988 2.87-2.87.033-.738.04-.96.04-2.829s-.007-2.09-.04-2.828c-.088-1.883-.973-2.783-2.87-2.87zm-2.829 9.293c-1.985 0-3.595-1.609-3.595-3.595 0-1.985 1.61-3.594 3.595-3.594s3.595 1.609 3.595 3.594c0 1.985-1.61 3.595-3.595 3.595zm3.737-6.491c-.464 0-.84-.376-.84-.84 0-.464.376-.84.84-.84.464 0 .84.376.84.84 0 .463-.376.84-.84.84zm-1.404 2.896c0 1.289-1.045 2.333-2.333 2.333s-2.333-1.044-2.333-2.333c0-1.289 1.045-2.333 2.333-2.333s2.333 1.044 2.333 2.333zm-2.333-12c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.958 14.886c-.115 2.545-1.532 3.955-4.071 4.072-.747.034-.986.042-2.887.042s-2.139-.008-2.886-.042c-2.544-.117-3.955-1.529-4.072-4.072-.034-.746-.042-.985-.042-2.886 0-1.901.008-2.139.042-2.886.117-2.544 1.529-3.955 4.072-4.071.747-.035.985-.043 2.886-.043s2.14.008 2.887.043c2.545.117 3.957 1.532 4.071 4.071.034.747.042.985.042 2.886 0 1.901-.008 2.14-.042 2.886z"/></svg></a>
+          </Link>
+        
+        </div>
+    </div>
+  );
+}
