8b41dcdf786cdde14031625d7141fb447fcd31dc
Terran Kartik Tellus <91951646+TerranKartikTellus@users.noreply.github.com>
2022-08-20 16:45:16 +0530

    fetch bg video from firebase-mumbai server

--- pages/_app.js
+++ pages/_app.js
@@ -17,13 +17,13 @@ function MyApp({ Component, pageProps }) {
 
 
     <video id="myVideo" preload="auto"  className="bg-cover opacity-50 saturate-200  hidden md:block object-cover w-full h-full absolute top-0 left-0"  autoPlay loop muted >
-      <source  src="/live/mylivewallpapers.com-Spaceman-with-Dog.mp4" type="video/mp4" />
+      <source rel="preload" src="https://firebasestorage.googleapis.com/v0/b/autoz-8611b.appspot.com/o/mylivewallpapers.com-Spaceman-with-Dog.mp4?alt=media&token=6138d067-509e-4fa1-8115-f022032be9fd" type="video/mp4" />
       {/* <source src="movie.ogg" type="video/ogg" /> */}
       <div className='bg-backgroundLG h-full w-full bg-cover'></div>
     </video>
 
     <video id="myVideo" preload="auto" className="bg-cover opacity-50 saturate-200  block md:hidden object-cover w-full h-full  absolute top-0 left-0"  autoPlay loop muted >
-      <source src="/live/MOBILE-Spaceman-with-Dog.mp4" type="video/mp4" />
+      <source src="https://firebasestorage.googleapis.com/v0/b/autoz-8611b.appspot.com/o/MOBILE-Spaceman-with-Dog.mp4?alt=media&token=26c4e4aa-bffa-4798-83e1-66913933164d" type="video/mp4" />
       {/* <source src="movie.ogg" type="video/ogg" /> */}
       <div className='bg-backgroundSM h-full  w-full bg-cover'></div>
     </video>
