a667cee86c91bd5ec5ad5c576000acf29df68a79
Terran Kartik Tellus <91951646+TerranKartikTellus@users.noreply.github.com>
2022-08-20 16:48:53 +0530

    added preload to img

--- pages/_app.js
+++ pages/_app.js
@@ -19,7 +19,7 @@ function MyApp({ Component, pageProps }) {
     <video id="myVideo" preload="auto"  className="bg-cover opacity-50 saturate-200  hidden md:block object-cover w-full h-full absolute top-0 left-0"  autoPlay loop muted >
       <source rel="preload" src="https://firebasestorage.googleapis.com/v0/b/autoz-8611b.appspot.com/o/mylivewallpapers.com-Spaceman-with-Dog.mp4?alt=media&token=6138d067-509e-4fa1-8115-f022032be9fd" type="video/mp4" />
       {/* <source src="movie.ogg" type="video/ogg" /> */}
-      <div className='bg-backgroundLG h-full w-full bg-cover'></div>
+      <div rel="preload"  className='bg-backgroundLG h-full w-full bg-cover'></div>
     </video>
 
     <video id="myVideo" preload="auto" className="bg-cover opacity-50 saturate-200  block md:hidden object-cover w-full h-full  absolute top-0 left-0"  autoPlay loop muted >
@@ -27,7 +27,7 @@ function MyApp({ Component, pageProps }) {
       {/* <source src="movie.ogg" type="video/ogg" /> */}
       <div className='bg-backgroundSM h-full  w-full bg-cover'></div>
     </video>
-    <div className='bg-black/40 w-full  h-full absolute top-0 left-0 animate-'>.</div>
+    <div rel="preload" className='bg-black/40 w-full  h-full absolute top-0 left-0 animate-'>.</div>
 
     <div className='scale-90 rounded-full shadowd shadow-gray-400/80 drop-shadow-2xl'>
         <div className='hden'>
