d7f4fe62c97a5add6424e94d4e2a2b56ecd9590a
Terran Kartik Tellus <91951646+TerranKartikTellus@users.noreply.github.com>
2022-09-01 23:05:28 +0530

    top nav of each page to scale 100%

--- components/common/top.js
+++ components/common/top.js
@@ -1,6 +1,6 @@
 export default function Top({main,sub,mini}){
   return(
-    <main className="fixed md:top-14 md:left-14 top-5 left-auto right-auto  w-full scale-75 md:scale-100 ">
+    <main className="fixed md:top-14 md:left-14 top-5 left-auto right-auto  w-full scale-100 md:scale-100 ">
       <div className="text-3xl tracking-widest text-blue-100 w-[200px]  text-center mx-auto md:mx-0 md:text-left  bg-red300 font-medium border-b-2 border-gray-100/30 pb-3 mb-3">{main}</div>
       <div className="flex flex-row items-start justify-start w-[100px] b-red-400 mx-auto md:mx-0">
       <div className="text-lg tracking-widest text-blue-100/80 text-center md:text-left w-full bg-red300 font-normal">{sub} </div>
